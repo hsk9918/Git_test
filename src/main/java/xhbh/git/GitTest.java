84288c808b5734a1667477e61bfc8027bfe3e327
hsk <hsk@pku.edu.com>
2022-12-02 18:55:21 +0800

    first commit

---
+++ src/main/java/xhbh/git/GitTest.java
@@ -0,0 +1,14 @@
+package xhbh.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author 小黄不黄
+ * @Date 2022/12/2 18:40
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
